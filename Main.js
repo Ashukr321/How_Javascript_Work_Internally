480f7a066210b9daeb5fdf8f65e2a8e50cf25663
Ashutosh kumar <115258392+Ashukr321@users.noreply.github.com>
2026-01-13 06:10:49 +0000

    feat:understand the the higher overview of the javascript

---
+++ Main.js
@@ -0,0 +1,19 @@
+// Higher overview of  the javascript 
+/*
+  single threaded just in time compilation 
+  support multi pradigm : approch to code 
+    1 .procedular 
+    2. object oriented 
+    3. functional programming 
+  memory management : dynamic memory management 
+    automatically allocate and eacclocate memeory  
+  5. garbage collected : cleaner if variable not used then gc : (remove from memeory ) 
+  6. non blocking event loop : 
+      handle all heavy task async task   using event loop 
+  7. first class function  : means function is assign into the variables 
+      function is also teated as variable 
+  8. Prototype based inheritance :
+    : in javascript  everythings is the object rather than primitive data 
+  9. dynamic typing : In javascript we don't need to assign the type of the variable it autotmatically identify the variable data types : 
+  in c and c++ and java we need to manualy defiend the types : 
+*/ 
